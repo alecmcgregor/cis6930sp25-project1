078fa41018698c68e8aa51fb37951c8ff92f8a23
alecmcgregor <120531848+alecmcgregor@users.noreply.github.com>
2025-03-01 23:39:22 -0500

    Update test_compare_remove_print.py

--- tests/test_compare_remove_print.py
+++ tests/test_compare_remove_print.py
@@ -23,11 +23,71 @@ dates = [
     }
 ]
 
+new_dates = [
+    {
+        "id": "224010123",
+        "accident_date": "2024-02-15",
+        "totalpeopleinvolved": "4",
+        "longitude": "-82.34012",
+        "latitude": "79.67545"
+    },
+    {
+        "id": "224010436",
+        "accident_date": "2024-02-15",
+        "totalpeopleinvolved": "3",
+        "longitude": "-82.34557",
+        "latitude": "79.67890"
+    }
+]
+
+crime_dates = [
+    {
+        "id": "224010123",
+        "accident_date": "2024-02-15",
+        "longitude": "-82.34012",
+        "latitude": "79.67545",
+    },
+    {
+        "id": "224010436",
+        "accident_date": "2024-02-15",
+        "longitude": "-82.34557",
+        "latitude": "79.67890"
+    }
+]
+
+expected_crime_dates = [
+    {
+        "id": "224010123",
+        "totalpeopleinvolved": "1",
+        "accident_date": "2024-02-15",
+        "longitude": "-82.34012",
+        "latitude": "79.67545",
+    },
+    {
+        "id": "224010436",
+        "totalpeopleinvolved": "1",
+        "accident_date": "2024-02-15",
+        "longitude": "-82.34557",
+        "latitude": "79.67890"
+    }
+]
+
 dates_df = pd.DataFrame(dates)
+new_dates_df = pd.DataFrame(new_dates)
+crime_dates_df = pd.DataFrame(crime_dates)
+expected_crime_dates_df = pd.DataFrame(expected_crime_dates)
 
 def test_compare_remove():
     testing = main.compare_location(location, dates_df)
     assert testing.equals(dates_df)
+
+def test_clean_crimes():
+    new_df = main.clean_crimes(crime_dates_df)
+    assert new_df.equals(expected_crime_dates_df)
+
+def test_clean_traffic():
+    new_df = main.clean_traffic(dates_df)
+    assert new_df.equals(new_dates_df)
     
 def test_printing():
     statement = io.StringIO()
