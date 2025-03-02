9dece6fc2729a14fd711376cbfe5118609b93365
alecmcgregor <120531848+alecmcgregor@users.noreply.github.com>
2025-03-01 23:23:04 -0500

    Update test_load_clean_parse.py

--- tests/test_load_clean_parse.py
+++ tests/test_load_clean_parse.py
@@ -3,102 +3,37 @@ import main
 import json
 import pandas as pd
 
-url = "https://data.cityofgainesville.org/resource/iecn-3sxx.json"
+year = 2024
+month = 12
+day = 12
 
-data = [
-    {
-        "case_number": "224010123",
-        "accident_date": "2024-02-15T14:35:22.000",
-        "totalpeopleinvolved": "4",
-        "longitude": "-82.34012",
-        "latitude": "79.67545"
-    },
-    {
-        "case_number": "224010456",
-        "accident_date": "2024-02-18T09:12:48.000",
-        "totalpeopleinvolved": "2",
-        "longitude": "-82.34567",
-        "latitude": "29.67890"
-    },
-    {
-        "case_number": "224010436",
-        "accident_date": "2024-02-28T09:12:48.000",
-        "totalpeopleinvolved": "1",
-        "longitude": "-12.34567",
-        "latitude": "79.67890"
-    },
-    {
-        "case_number": "224010436",
-        "accident_date": "2024-02-15T09:12:48.000",
-        "totalpeopleinvolved": "3",
-        "longitude": "-82.34557",
-        "latitude": "79.67890"
-    }
-]
+date = "2024-12-12"
 
-df = pd.DataFrame(data)
+url_traffic = "https://data.cityofgainesville.org/resource/iecn-3sxx.json"
 
-cleaned = [
-    {
-        "case_number": "224010123",
-        "accident_date": "2024-02-15",
-        "totalpeopleinvolved": "4",
-        "longitude": "-82.34012",
-        "latitude": "79.67545"
-    },
-    {
-        "case_number": "224010456",
-        "accident_date": "2024-02-18",
-        "totalpeopleinvolved": "2",
-        "longitude": "-82.34567",
-        "latitude": "29.67890"
-    },
-    {
-        "case_number": "224010436",
-        "accident_date": "2024-02-28",
-        "totalpeopleinvolved": "1",
-        "longitude": "-12.34567",
-        "latitude": "79.67890"
-    },
-    {
-        "case_number": "224010436",
-        "accident_date": "2024-02-15",
-        "totalpeopleinvolved": "3",
-        "longitude": "-82.34557",
-        "latitude": "79.67890"
-    }
-]
+url_crime = "https://data.cityofgainesville.org/resource/gvua-xt9q.json"
 
-clean_df = pd.DataFrame(cleaned)
+expected_traffic = f"https://data.cityofgainesville.org/resource/iecn-3sxx.json?$where=accident_date%20between%20%27{date}T00:00:00%27%20and%20%27{date}T23:59:59%27"
 
-dates = [
-    {
-        "case_number": "224010123",
-        "accident_date": "2024-02-15",
-        "totalpeopleinvolved": "4",
-        "longitude": "-82.34012",
-        "latitude": "79.67545"
-    },
-    {
-        "case_number": "224010436",
-        "accident_date": "2024-02-15",
-        "totalpeopleinvolved": "3",
-        "longitude": "-82.34557",
-        "latitude": "79.67890"
-    }
-]
+expected_crime = f"https://data.cityofgainesville.org/resource/gvua-xt9q.json?$where=report_date%20between%20%27{date}T00:00:00%27%20and%20%27{date}T23:59:59%27"
 
-dates_df = pd.DataFrame(dates)
+def test_formatdate():
+    new_date = main.formatdate(year,month,day)
+    assert new_date == "2024-12-12"
+
+def test_formattraffic_url():
+    url = main.format_traffic_url(url_traffic, date)
+    assert url == expected_traffic
+
+def test_formatcrime_url():
+    url = main.format_crime_url(url_crime, date)
+    assert url == expected_crime
 
 def test_download_url():
-    df_1 = main.fetchdata(url)
+    df_1 = main.fetchdata(expected_traffic)
     assert not df_1.empty
 
-def test_clean_data():
-    clean_data = main.data_cleaning(df)
-    assert clean_data.equals(clean_df)
-
-def test_date_filtering():
-    just_dates = main.filterdates(clean_df, "2024-02-15")
-    just_dates = just_dates.reset_index(drop=True)
-    assert just_dates.equals(dates_df)
+def test_correct_date():
+    df = main.fetchdata(expected_traffic)
+    df['accident_date'] = df['accident_date'].str.replace(r'T.*', '', regex=True)
+    assert (df['accident_date'] == date).all()
